3af07db82cb2c450962437abcfbb533f75e37c56
Jason Balicki <jbalicki@centene.com>
2016-04-21 13:01:14 -0500

    adds name => to user def in lockdown_users.pp

--- manifests/lockdown_users.pp
+++ manifests/lockdown_users.pp
@@ -2,6 +2,7 @@
 define os_hardening::lockdown_users {
   user { "osh_user_${title}":
     ensure   => present,
+    name     => $title,
     shell    => '/usr/sbin/nologin',
     password => '*',
   }
